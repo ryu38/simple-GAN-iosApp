d7660a9a2777937a2d9a84aed888b00eb3c281b3
久賀隆矢 <niceneetlife.ryu@gmail.com>
2021-12-24 09:57:07 +0900

    Initial Commit

---
+++ Shared/TryCoreMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryCoreMLApp.swift
+//  Shared
+//
+//  Created by no145 on 2021/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TryCoreMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
